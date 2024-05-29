c5bedd893b786617427448402855504ee9334fce
dolag <dolag233@gmail.com>
2024-05-29 22:35:05 +0800

    [Fix] Config qt platform

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import sys
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "panel"))
+qt_plugin_path = os.path.join(os.path.dirname(sys.executable), r"Lib\site-packages\PySide2\plugins")
+os.environ["path"] += os.path.join(qt_plugin_path, "platforms")
+os.environ["QT_PLUGIN_PATH"] = qt_plugin_path
+
 from PySide2.QtWidgets import QApplication, QMessageBox
 import qdarktheme
 qdarktheme.enable_hi_dpi()
