e0928cabad139c199745a5a8b2c0d52ffcc0a173
dolag <dolag233@gmail.com>
2024-06-02 16:43:07 +0800

    [Fix] Fix incorrect parameters in batch update

--- panel/hou_parms_model.py
+++ panel/hou_parms_model.py
@@ -169,6 +169,7 @@ class HouParmsModel(QObject):
         parm_metas = []
         for index_or_key, parm_value in key_value_dict.items():
             parm_meta = self.getParmMeta(index_or_key)
+            parm_meta.setDataSpecific(HouParamMetaEnum.VALUE, parm_value)
             parm_metas.append(parm_meta)
 
         self.view_datas_changed.emit(parm_metas)
